6047e67157d4decfe996e6dd7489b3c8dfffee24
dahyun-shuket <dh.yang@shuket.company>
2022-12-09 09:37:53 +0900

    npm install 변경

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 # cp -r /var/lib/jenkins/workspace/node-test /var/www/jenkins.test
-cp -R `ls .|grep -v .git` /var/www/jenkins.test/node-test
 npm install
+cp -R `ls .|grep -v .git` /var/www/jenkins.test/node-test
 pm2 restart all
